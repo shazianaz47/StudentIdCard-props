e0ba3502ad6da4c5514ce0c4b240a1e12eafd857
shazia naz <shazianaz2417@gmail.com>
2025-02-15 16:20:52 +0500

    making card

--- src/app/Cmponents/Card.tsx
+++ src/app/Cmponents/Card.tsx
@@ -1,16 +1,25 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import AbdulRafay from "../../public/images/AbdulRafay.jpg"
 
-const Card = ({name, age, rollNo, Class}) => {
+interface CardProps {
+  name: string;
+  age: number;
+  rollNo: string;
+  studentClass: string;
+}
+
+
+const Card:React.FC<CardProps> = ({name, age, rollNo, studentClass}) => {
   return (
     <div className="bg-blue-white rounded-lg shadow-md p-3 max-w-screen-sm mx-auto my-2">
-      <Image src={AbdulRafay} alt="picture Abdul Rafay "/>
+      <Image src={AbdulRafay} alt="picture Abdul Rafay  width={40} heigh={40}"/>
 
       <h2 className="flex-auto text-xl font-bold text-gray-800 mb-2">Name:{name}</h2>
       <p className="text-gray-600 mb-2">Age:{age} </p>
       <p className="text-gray-600 mb-2">Roll No:{rollNo}</p>
-      <p className="text-gray-600">Class:{Class} </p>
+      <p className="text-gray-600">StudentClass:{studentClass} </p>
       
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,30 @@
- import React from "react"
-  import Card from "./card/page";
+ import React from "react";
+  import Card from "@/app/Cmponents/Card";
  
 
 //props
 export default function Home() {
 
-      const student =[
-       {name:'Shan',age:6 ,rollno:'S1234',class:'3rd Grade', },
-       {name:'Nabeel', age:8 ,rollno:'S1235' ,class:'5th Grade', },
-       {name:'Burhan',age:10 ,rollno:'S1236',class:'6th Grade' ,},
-       {name:'Ali', age: 5 ,rollno:'S1237' ,class:'2nd Grade',},
+      const students =[
+       {name:'Shan',age:6 ,rollno:'S1234',studentClass:'3rd Grade', },
+       {name:'Nabeel', age:8 ,rollno:'S1235' ,studentClass:'5th Grade', },
+       {name:'Burhan',age:10 ,rollno:'S1236',studentClass:'6th Grade' ,},
+       {name:'Ali', age: 5 ,rollno:'S1237' ,studentClass:'2nd Grade',},
       ];
 return (
     <div className="flex flex-col items-center mt-10">               
       <h1 className="text-3xl font-bold mb-6">Student ID Card</h1>
 
-        {student.map((student,index) => (
+        {students.map((student, index) => (
           < Card
             key={index}
             name={student.name}
             age={student.age}
             rollNo={student.rollno}
-            Class={student.class}
-            // image={student.image}
+            studentClass={student.studentClass}
           />
         ))}
       </div>
-    
   );
 };
 
